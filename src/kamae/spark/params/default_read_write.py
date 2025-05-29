7f3d9d72ce233f88e24dfeb21700fceaebfd9cfd
George Barrowclough <george.d.b@hotmail.com>
2025-05-29 12:08:05 +0100

    chore: Add license header

--- src/kamae/spark/params/default_read_write.py
+++ src/kamae/spark/params/default_read_write.py
@@ -1,3 +1,17 @@
+# Copyright [2024] Expedia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Literally a copy of pyspark.ml.util DefaultParamsWritable/Readable/Writer/Reader.
 This is needed because Databricks runtimes > 13.3LTS have slow metadata writes because
 they have introduced this v4.0.0 change: https://github.com/apache/spark/pull/47453
