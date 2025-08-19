63039efc0afcd6e05d2ccdfcfedf2d9c1b4b9125
James Shinner <jshinner@expediagroup.com>
2025-08-19 16:33:25 +0100

    feat: Add transformer and layer

---
+++ src/kamae/spark/transformers/list_rank.py
@@ -0,0 +1,127 @@
+# Copyright [2024] Expedia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional
+
+import pyspark.sql.functions as F
+import tensorflow as tf
+from pyspark import keyword_only
+from pyspark.sql import DataFrame, Window
+from pyspark.sql.types import (
+    ByteType,
+    DataType,
+    DoubleType,
+    FloatType,
+    IntegerType,
+    LongType,
+    ShortType,
+)
+
+from kamae.spark.params import (
+    ListwiseParams,
+    SingleInputSingleOutputParams,
+)
+from kamae.spark.utils import check_listwise_columns
+from kamae.tensorflow.layers import ListRankLayer
+
+from .base import BaseTransformer
+
+
+class ListRankTransformer(
+    BaseTransformer,
+    SingleInputSingleOutputParams,
+    ListwiseParams,
+):
+    """
+    Calculate the listwise rank across the query id column.
+
+    Example: calculate the rank of items within a query, given the score.
+
+    :param inputCol: Value column, on which to calculate the rank.
+    :param outputCol: Name of output col.
+    :param inputDtype: Data Type of input.
+    :param outputDtype: Data Type of output.
+    :param layerName: The name of the transformer, which typically
+    should be the name of the produced feature.
+    :param queryIdCol: Name of column to aggregate upon. It is required.
+    """
+
+    @keyword_only
+    def __init__(
+        self,
+        inputCol: Optional[str] = None,
+        outputCol: Optional[str] = None,
+        inputDtype: Optional[str] = None,
+        outputDtype: Optional[str] = None,
+        layerName: Optional[str] = None,
+        queryIdCol: Optional[str] = None,
+    ) -> None:
+        super().__init__()
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @property
+    def compatible_dtypes(self) -> Optional[List[DataType]]:
+        """
+        List of compatible data types for the layer.
+        If the computation can be performed on any data type, return None.
+
+        :returns: List of compatible data types for the layer.
+        """
+        return [
+            FloatType(),
+            DoubleType(),
+            ByteType(),
+            ShortType(),
+            IntegerType(),
+            LongType(),
+        ]
+
+    def _transform(self, dataset: DataFrame) -> DataFrame:
+        """
+        Calculate the rank.
+        :param dataset: The dataframe with signals and features.
+        :returns: The dataframe dataset with the new feature.
+        """
+        if not self.isDefined("queryIdCol"):
+            raise ValueError("queryIdCol must be set on listwise transformers.")
+
+        check_listwise_columns(
+            dataset=dataset,
+            query_col_name=self.getQueryIdCol(),
+            value_col_name=self.getInputCol(),
+            sort_col_name=None,
+        )
+
+        # Define window spec
+        window_spec = Window.partitionBy(self.getQueryIdCol()).orderBy(self.getInputCol())
+
+        # Calculate the rank
+        dataset = dataset.withColumn(self.getOutputCol(), F.row_number().over(window_spec))
+
+        return dataset
+
+    def get_tf_layer(self) -> tf.keras.layers.Layer:
+        """
+        Gets the tensorflow layer for the listwise-rank transformer.
+
+        :returns: Tensorflow keras layer with name equal to the layerName parameter that
+         performs an averaging operation.
+        """
+        return ListRankLayer(
+            name=self.getLayerName(),
+            input_dtype=self.getInputTFDtype(),
+            output_dtype=self.getOutputTFDtype(),
+            axis=1,
+        )
---
+++ src/kamae/tensorflow/layers/list_rank.py
@@ -0,0 +1,106 @@
+# Copyright [2024] Expedia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Dict, Iterable, List, Optional
+
+import tensorflow as tf
+
+import kamae
+from kamae.tensorflow.typing import Tensor
+from kamae.tensorflow.utils import enforce_single_tensor_input
+
+from .base import BaseLayer
+
+
+@tf.keras.utils.register_keras_serializable(package=kamae.__name__)
+class ListRankLayer(BaseLayer):
+    """
+    Calculate the rank across the axis dimension.
+
+    Example: calculate the rank of items within a query, given the score.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        input_dtype: str = None,
+        output_dtype: str = None,
+        axis: int = 1,
+        **kwargs,
+    ):
+        """
+        Initializes the Listwise Rank layer.
+
+        WARNING: The code is fully tested for axis=1 only. Further testing is needed.
+
+        :param name: Name of the layer, defaults to `None`.
+        :param input_dtype: The dtype to cast the input to. Defaults to `None`.
+        :param output_dtype: The dtype to cast the output to. Defaults to `None`.
+        :param top_n: The number of top items to consider when calculating the min.
+        :param sort_order: The order to sort the second tensor by. Defaults to `asc`.
+        :param min_filter_value: The minimum filter value to ignore values during
+        calculation. Defaults to None (no filter).
+        :param nan_fill_value: The value to fill NaNs results with. Defaults to 0.
+        :param axis: The axis to calculate the statistics across. Defaults to 1.
+        """
+        super().__init__(
+            name=name, input_dtype=input_dtype, output_dtype=output_dtype, **kwargs
+        )
+        self.axis = axis
+
+    @property
+    def compatible_dtypes(self) -> Optional[List[tf.dtypes.DType]]:
+        """
+        Returns the compatible dtypes of the layer.
+
+        :returns: The compatible dtypes of the layer.
+        """
+        return [
+            tf.bfloat16,
+            tf.float16,
+            tf.float32,
+            tf.float64,
+            tf.uint8,
+            tf.int8,
+            tf.uint16,
+            tf.int16,
+            tf.int32,
+            tf.int64,
+        ]
+
+    @enforce_single_tensor_input
+    def _call(self, inputs: Iterable[Tensor], **kwargs) -> Tensor:
+        """
+        Calculate the rank.
+
+        :param inputs: The iterable tensor for the feature.
+        :returns: Thew new tensor result column.
+        """
+
+        return tf.math.add(tf.argsort(tf.argsort(inputs, axis=self.axis), axis=self.axis), 1)
+
+    def get_config(self) -> Dict[str, Any]:
+        """
+        Gets the configuration of the layer.
+        Used for saving and loading from a model.
+
+        :returns: Dictionary of the configuration of the layer.
+        """
+        config = super().get_config()
+        config.update(
+            {
+                "axis": self.axis,
+            }
+        )
+        return config
