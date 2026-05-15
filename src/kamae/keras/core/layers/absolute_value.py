f8f58408158da38d115b092da0f8f3d9f2ef8192
George Barrowclough <george.d.b@hotmail.com>
2026-05-15 12:36:38 +0100

    feat: Move Absolute Layer to codegen

--- src/kamae/keras/core/layers/absolute_value.py
+++ src/kamae/keras/core/layers/absolute_value.py
@@ -12,18 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, List, Optional
+from typing import Any
 
-import keras
 from keras import ops
 
-import kamae
 from kamae.keras.core.base import BaseLayer
 from kamae.keras.core.typing import Tensor
 from kamae.keras.core.utils.input_utils import enforce_single_tensor_input
 
 
-@keras.saving.register_keras_serializable(package=kamae.__name__)
 class AbsoluteValueLayer(BaseLayer):
     """
     Performs the abs(x) operation on a given input tensor.
@@ -31,62 +28,23 @@ class AbsoluteValueLayer(BaseLayer):
 
     jit_compatible = True
 
-    def __init__(
-        self,
-        name: Optional[str] = None,
-        input_dtype: Optional[str] = None,
-        output_dtype: Optional[str] = None,
-        **kwargs: Any,
-    ) -> None:
-        """
-        Initializes the AbsoluteValueLayer layer
-
-        :param name: Name of the layer, defaults to `None`.
-        :param input_dtype: The dtype to cast the input to. Defaults to `None`.
-        :param output_dtype: The dtype to cast the output to. Defaults to `None`.
-        """
-        super().__init__(
-            name=name, input_dtype=input_dtype, output_dtype=output_dtype, **kwargs
-        )
-
-    @property
-    def compatible_dtypes(self) -> Optional[List[str]]:
-        """
-        Returns the compatible dtypes of the layer.
-
-        :returns: List of compatible dtype names
-        """
-        return [
-            "float16",
-            "float32",
-            "float64",
-            "int32",
-            "int64",
-            "complex64",
-            "complex128",
-        ]
+    _compatible_dtypes = [
+        "float16",
+        "float32",
+        "float64",
+        "int32",
+        "int64",
+        "complex64",
+        "complex128",
+    ]
 
     @enforce_single_tensor_input
     def _call(self, inputs: Tensor, **kwargs: Any) -> Tensor:
         """
         Performs the abs(x) operation on a given input tensor.
 
-        Decorated with `@enforce_single_tensor_input` to ensure that the input
-        is a single tensor. Raises an error if multiple tensors are passed
-        in as an iterable.
 
         :param inputs: Tensor to perform the abs(x) operation on.
         :returns: The absolute value of the input tensor.
         """
-        outputs = ops.absolute(inputs)
-        return outputs
-
-    def get_config(self) -> Dict[str, Any]:
-        """
-        Gets the configuration of the AbsoluteValue layer.
-        Used for saving and loading from a model.
-
-        :returns: Dictionary of the configuration of the layer.
-        """
-        config = super().get_config()
-        return config
+        return ops.absolute(inputs)
