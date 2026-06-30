4e367d3797f7effd981400122cd453d6351e854d
Conor worthington <45695214+ConorWorthington@users.noreply.github.com>
2026-06-30 17:37:11 +0100

    Update standard_scale.py

--- src/kamae/spark/estimators/standard_scale.py
+++ src/kamae/spark/estimators/standard_scale.py
@@ -21,15 +21,10 @@ from typing import List, Optional
 import numpy as np
 import pyspark.sql.functions as F
 from pyspark import keyword_only
-from pyspark.sql import DataFrame
 from pyspark.sql.types import ArrayType, DataType, DoubleType, FloatType
+from pyspark.storagelevel import StorageLevel
 
-from kamae.keras.core.backend import ALL_BACKENDS
-from kamae.spark.params import (
-    MaskValueParams,
-    SampleFractionParams,
-    SingleInputSingleOutputParams,
-)
+from kamae.spark.params import MaskValueParams, SingleInputSingleOutputParams
 from kamae.spark.transformers import StandardScaleTransformer
 from kamae.spark.utils import construct_nested_elements_for_scaling
 
@@ -38,7 +33,6 @@ from .base import BaseEstimator
 
 class StandardScaleEstimator(
     BaseEstimator,
-    SampleFractionParams,
     SingleInputSingleOutputParams,
     MaskValueParams,
 ):
@@ -47,13 +41,7 @@ class StandardScaleEstimator(
     This estimator is used to calculate the mean and standard deviation of the input
     feature column. When fit is called it returns a StandardScaleTransformer
     which can be used to standardize/transform additional features.
-
-    WARNING: If the input is an array, we assume that the array has a constant
-    shape across all rows.
     """
-
-    supported_backends = ALL_BACKENDS
-    jit_compatible = True
 
     @keyword_only
     def __init__(
@@ -64,7 +52,6 @@ class StandardScaleEstimator(
         outputDtype: Optional[str] = None,
         layerName: Optional[str] = None,
         maskValue: Optional[float] = None,
-        sampleFraction: Optional[float] = None,
     ) -> None:
         """
         Initializes a StandardScaleEstimator estimator.
@@ -78,12 +65,10 @@ class StandardScaleEstimator(
         transforming.
         :param layerName: Name of the layer. Used as the name of the tensorflow layer
          in the keras model. If not set, we use the uid of the Spark transformer.
-        :param sampleFraction: Fraction of data to sample for statistics
-         estimation (exclusive 0.0-1.0). Default None (no sampling).
         :returns: None - class instantiated.
         """
         super().__init__()
-        self._setDefault(maskValue=None, sampleFraction=None)
+        self._setDefault(maskValue=None)
         kwargs = self._input_kwargs
         self.setParams(**kwargs)
 
@@ -97,7 +82,7 @@ class StandardScaleEstimator(
         """
         return [FloatType(), DoubleType()]
 
-    def _fit(self, dataset: DataFrame) -> "StandardScaleTransformer":
+    def _fit(self, dataset) -> "StandardScaleTransformer":
         """
         Fits the StandardScaleEstimator estimator to the given dataset.
         Calculates the mean and standard deviation of the input feature column and
@@ -113,41 +98,54 @@ class StandardScaleEstimator(
         else:
             input_col = F.col(self.getInputCol())
 
-        # Collect a single row to driver and get the length.
-        # We assume all subsequent rows have the same length.
-        array_size = np.array((dataset.select(input_col).first()[0])).shape[-1]
+        # Persist so the array-size probe and the moments aggregation reuse a
+        # materialised result instead of re-scanning the upstream lineage twice.
+        # Guarded so we do not double-persist data the caller already cached.
+        already_cached = dataset.storageLevel.useMemory or dataset.storageLevel.useDisk
+        if not already_cached:
+            dataset = dataset.persist(StorageLevel.MEMORY_AND_DISK)
 
-        element_struct = construct_nested_elements_for_scaling(
-            column=input_col,
-            column_datatype=input_column_type,
-            array_dim=array_size,
-        )
+        try:
+            # Collect a single row to driver and get the length.
+            # We assume all subsequent rows have the same length.
+            array_size = np.array((dataset.select(input_col).first()[0])).shape[-1]
 
-        mean_cols = [
-            F.mean(
-                F.when(
-                    F.col(f"element_struct.element_{i}") == F.lit(self.getMaskValue()),
-                    F.lit(None),
-                ).otherwise(F.col(f"element_struct.element_{i}"))
-            ).alias(f"mean_{i}")
-            for i in range(1, array_size + 1)
-        ]
+            element_struct = construct_nested_elements_for_scaling(
+                column=input_col,
+                column_datatype=input_column_type,
+                array_dim=array_size,
+            )
 
-        stddev_cols = [
-            F.stddev_pop(
-                F.when(
-                    F.col(f"element_struct.element_{i}") == F.lit(self.getMaskValue()),
-                    F.lit(None),
-                ).otherwise(F.col(f"element_struct.element_{i}"))
-            ).alias(f"stddev_{i}")
-            for i in range(1, array_size + 1)
-        ]
+            mean_cols = [
+                F.mean(
+                    F.when(
+                        F.col(f"element_struct.element_{i}")
+                        == F.lit(self.getMaskValue()),
+                        F.lit(None),
+                    ).otherwise(F.col(f"element_struct.element_{i}"))
+                ).alias(f"mean_{i}")
+                for i in range(1, array_size + 1)
+            ]
 
-        metric_cols = mean_cols + stddev_cols
+            stddev_cols = [
+                F.stddev_pop(
+                    F.when(
+                        F.col(f"element_struct.element_{i}")
+                        == F.lit(self.getMaskValue()),
+                        F.lit(None),
+                    ).otherwise(F.col(f"element_struct.element_{i}"))
+                ).alias(f"stddev_{i}")
+                for i in range(1, array_size + 1)
+            ]
 
-        mean_and_stddev_dict = (
-            dataset.select(element_struct).agg(*metric_cols).first().asDict()
-        )
+            metric_cols = mean_cols + stddev_cols
+
+            mean_and_stddev_dict = (
+                dataset.select(element_struct).agg(*metric_cols).first().asDict()
+            )
+        finally:
+            if not already_cached:
+                dataset.unpersist()
         mean = [mean_and_stddev_dict[f"mean_{i}"] for i in range(1, array_size + 1)]
         stddev = [mean_and_stddev_dict[f"stddev_{i}"] for i in range(1, array_size + 1)]
 
